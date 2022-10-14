67274c07f4f8e2a20ef9743bf86a32523dfe92f1
heckerpowered <72550834+heckerpowered@users.noreply.github.com>
2022-10-14 17:00:54 +0800

    Backup

--- Steroids/Source/Steroids/Private/Transmit/Transmit.cpp
+++ Steroids/Source/Steroids/Private/Transmit/Transmit.cpp
@@ -13,4 +13,23 @@ HandleTransmit(
 	// Ensures input buffer or output buffer is not null
 	ASSERT(InputBuffer != nullptr);
 	ASSERT(OutputBuffer != nullptr);
+
+	__try {
+		// Checks that a user-mode buffer actually resides in the user portion of the address space
+		ProbeForRead(InputBuffer, InputBufferLength, sizeof(CHAR));
+	}
+	__except (GetExceptionCode() == STATUS_ACCESS_VIOLATION ? EXCEPTION_EXECUTE_HANDLER : EXCEPTION_CONTINUE_SEARCH) {
+		return GetExceptionCode();
+	}
+
+	switch (IoGetFunctionCodeFromCtlCode(IoControlCode)) {
+	case 0:
+		OutputBufferLength = 1;
+
+		return STATUS_SUCCESS;
+		break;
+	default:
+		OutputBufferLength = 0;
+		return STATUS_ILLEGAL_FUNCTION;
+	}
 }
